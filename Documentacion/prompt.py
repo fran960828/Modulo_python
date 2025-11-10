6deb8afd923f9b448a537889acff6ba70cb41a1a
Francisco Navarro <franng96@hotmail.com>
2025-11-10 18:01:29 +0100

    datetime

---
+++ Documentacion/prompt.py
@@ -0,0 +1,7 @@
+'''
+genera un serie de ejercicios para practicar todos estos conceptos,
+estos ejercicios deben diferir de los los ejemplos de la documentación que he pasado y 
+aumentar progresivamente de dificultad hasta el punto de alcanzar un uso profesional y fluido.
+Genera el número de ejercicios que consideres necesario para alcanzar un buen nivel de dominio
+
+'''
